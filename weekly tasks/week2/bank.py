614c9cadf003b2ddfd36ab5826c530279ad64478
Svitlana <G00473000@atu.ie>
2025-04-25 18:48:23 +0100

    Update bank.py

--- weekly tasks/week2/bank.py
+++ weekly tasks/week2/bank.py
@@ -1,6 +1,8 @@
+# Ask the user to input each amount in cents
 amount1 = input("Enter amount1(in cent): ")
 amount2 = input("Enter amount2(in cent): ")
 
+# Convert both inputs from strings to integers and add them together
 sum = int(amount1) + int(amount2)
 
 print(f'The sum of these is \N{euro sign} {sum/100}')
