18cad5d881ac4ed2e83d3a392d2aed07507980f5
Svitlana <G00473000@atu.ie>
2025-04-25 15:28:21 +0100

    organize

---
+++ weekly tasks/week2/bank.py
@@ -0,0 +1,6 @@
+amount1 = input("Enter amount1(in cent): ")
+amount2 = input("Enter amount2(in cent): ")
+
+sum = int(amount1) + int(amount2)
+
+print(f'The sum of these is \N{euro sign} {sum/100}')
